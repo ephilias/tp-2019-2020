379ad1706201d35c29a130b6d98ff2281faadb45
Marc UNGERER <ungerer@etu.unistra.fr>
2020-01-10 15:47:03 +0100

    nodemailer

--- api/controllers/CowsayController.js
+++ api/controllers/CowsayController.js
@@ -6,6 +6,7 @@
  */
 
 var cowsay = require('cowsay');
+var nodemailer = require('nodemailer');
 
 module.exports = {
   /**
@@ -43,6 +44,46 @@ module.exports = {
 
   send: async function(req, res) {
     //@TODO send
+    nodemailer.createTransport({
+      host: "smtp://postmaster@mailgun.l3o.eu:fedbe91ae5e3529f94528dd311bea4c9-060550c6-d42c872f@smtp.mailgun.org:587",
+      port: 587,
+      secure: false, // upgrade later with STARTTLS
+      auth: {
+        user: "username",
+        pass: "password"
+      }
+    });
+
+    let count = await Sentences.count();
+    for (let i = 0; i < count; i++) {
+
+      let s = await Sentences.find().limit(i);
+      let sentence = "Random Message";
+      if(s.length > 0) {
+        sentence = s[0].sentence;
+      }
+
+      let email = "random@email.email"
+      if(s.length > 0) {
+        email = s[0].email;
+      }
+
+      const mailOptions = {
+        from: 'cdad@l3o.eu', // sender address
+        to: email, // list of receivers
+        subject: 'Sentence', // Subject line
+        html: '<p>' + sentence + '</p>'// plain text body
+      };
+
+      transporter.sendMail(mailOptions, function (err, info) {
+       if(err)
+         console.log(err)
+       else
+         console.log(info);
+      });
+    }
+
+
     return res.redirect('/say');
   },
 };
